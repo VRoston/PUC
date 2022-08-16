aeb56e3479d985ac2c0fdad1ded040ddd5a9389a
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-08-16 14:02:52 -0300

    Create 15.08.py

---
+++ Python/15.08.py
@@ -0,0 +1,25 @@
+'''
+Primeira aula de python
+15/08
+
+'''
+#Comentario de linhas
+
+from tkinter import CASCADE
+
+
+valor = 10
+
+print(valor)
+print("numero")
+
+print("numero", str(valor))
+print('numero (valor)')
+valor = "casa"
+print(valor)
+
+#ler teclado
+valorlido= input('Digite um valor entre 0 a 9:')
+soma=valorlido*10
+print(valorlido)
+print(f'Valor digitado multiplicado por 10: {soma}')
