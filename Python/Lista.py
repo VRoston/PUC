5dd23aacc243a7bede6c2e480f0f3bcbc949ced3
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-08-29 14:42:26 -0300

    Create Lista.py

---
+++ Python/Lista.py
@@ -0,0 +1,12 @@
+'''
+Exemplo de listas em python
+'''
+
+lista = ['Sao Paulo','Palmeiras','Santos','Portuguesa']
+print(lista)
+print(lista[2])
+print(lista[1:3])
+print(lista[3:4])
+print(lista[1:])
+print(lista[:2])
+print(lista[-1])
