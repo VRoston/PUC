1ebb2d21ba28d63ed8953ab88a5f78a30fb3c5a7
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-09-05 13:41:11 -0300

    Create 05.09.py

---
+++ Python/05.09.py
@@ -0,0 +1,19 @@
+print("Listas")
+
+lista = []       #Lista vazia
+
+lista.append(10)
+lista.insert(1,20)
+print(lista)
+lista.append(10)
+lista.insert(1,20)
+print(lista)
+#percorre a lista
+
+print(len(lista))
+for i in range(0,len(lista)):
+    print(list[i], end=' ')
+print()
+
+for item i lista:
+    print(item , end=' ')
