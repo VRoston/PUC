e6c1b2da44009ba0bced704c5bae6ffd8c3d6e37
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-09-12 14:21:47 -0300

    Update 12.09.py

--- Python/12.09.py
+++ Python/12.09.py
@@ -4,16 +4,23 @@ CRIANDO A BIBLIOTECA PARA ROBÓTICA COMPUTACIONAL
 
 import numpy as np
 
-#def cria_vetor(vlist: list)->np.arrey:
-
+#def cria_vetor3(vlist: list)->np.arrey:
 def cria_vetor3(vlist):
     #testa se a lista tem no minimo tres elementos
     if len(vlist)==3:
+        for item in vlist:
+            try:
+                n=int(item)
+            except:
+                raise ValueError('valores')
+        #cria matriz vazia com numpy
+        res=np.zeros([3,1])
+        for cl in range(0, len(vlist)):
+            res[c1,0]=vlist[cl]
         print('OK')
         return
 
-    raise ValueError("A sua lista dewveria ter 3 posições")
-
+    raise ValueError("A sua lista deveria ter 3 posições")
 
 #if('__name__'=='__main__'):
 print('Inicinado os tesstes')
@@ -26,5 +33,11 @@ try:
 except ValueError as ve:
     print('ops, deu erro')
     print(ve)
+
+try:
+    list3 = ['case','if','else']
+    cria_vetor3(list3)
+except ValueError as msg:
+    print(msg)
 
 print('Terminou os testes')
