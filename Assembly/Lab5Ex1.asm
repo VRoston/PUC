e758abcc7346b400d083b3ce11667f341669bb30
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-09-14 14:35:49 -0300

    ASM

--- Assembly/Lab5Ex1.asm
+++ Assembly/Lab5Ex1.asm
@@ -2,19 +2,13 @@
 .DATA
 .CODE
 MAIN PROC
-    MOV BL,5            ;BL recebe 5 para realizar o loop 5 vezes
-VOLTA1:
-    MOV DL,42           ;DL recebe '*' da tabela ascii
-    MOV BH,10           ;BH recebe o valor 10 para controlar a quantidade de asterisco na linha 
+    MOV CX,50           ;CX recebe 50 para realizar o loop 50 vezes
+    
 VOLTA:
         MOV AH,02       ;Imprimir na tela (Entrada DL, Retorna AL)
+        MOV DL,42       ;Código ascii do asterisco
         INT 21H         ;Executa a função de impressão na tela
-        dec BH          ;Decrementa BH (BH--) para quando chegar a 0 terminar o loop
-        JNZ VOLTA       ;Loop para imprimir os 10 asteriscos na mesma linha
-    MOV AH,01           ;Apertar qualquer tecla
-    INT 21H             ;Executa apertar qualquer tecla
-    ;DEC BL              ;Decrementa BL (BL--) para quando chegar a 0 terminar o loop
-    LOOP VOLTA1          ;Loop para imprimir os asteriscos na linha de baixo
+        LOOP VOLTA      ;Loop para imprimir os 10 asteriscos na mesma linha
 
     MOV AH,4CH          ;Finaliza o programa
     INT 21H             ;Executa a finalização
--- Assembly/Lab5Ex2Descarte.asm
+++ Assembly/Lab5Ex2Descarte.asm
@@ -5,15 +5,14 @@ MAIN PROC
     MOV CX,5            ;BL recebe 5 para realizar o loop 5 vezes
 VOLTA1:
     MOV DL,42           ;DL recebe '*' da tabela ascii
-    MOV BX,CX           ;Move CX em BX para não haver conflito com os dois loops
-    MOV CX,10           ;CX recebe o valor 10 para controlar a quantidade de asterisco na linha
+    MOV BH,10           ;BH recebe o valor 10 para controlar a quantidade de asterisco na linha 
 VOLTA:
         MOV AH,02       ;Imprimir na tela (Entrada DL, Retorna AL)
         INT 21H         ;Executa a função de impressão na tela
-        LOOP VOLTA      ;Loop para imprimir os 10 asteriscos na mesma linha
-    MOV CX,BX           ;Retorna o valor antigo de CX que estava salvo em BX
-    MOV AH,01           ;Entrade de um caractere
-    INT 21H             ;Executa a entrada de um caractere
+        dec BH          ;Decrementa BH (BH--) para quando chegar a 0 terminar o loop
+        JNZ VOLTA       ;Loop para imprimir os 10 asteriscos na mesma linha
+    MOV AH,01           ;Apertar qualquer tecla
+    INT 21H             ;Executa apertar qualquer tecla
     LOOP VOLTA1         ;Loop para imprimir os asteriscos na linha de baixo
 
     MOV AH,4CH          ;Finaliza o programa
