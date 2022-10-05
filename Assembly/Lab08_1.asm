51c5043116d62ac8d3ff28c0e60d4c06f7c1f537
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-10-05 14:45:47 -0300

    Update Lab08_1.asm

--- Assembly/Lab08_1.asm
+++ Assembly/Lab08_1.asm
@@ -4,22 +4,29 @@
 .DATA
 .CODE
 MAIN PROC
-    MOV AL,06
-    SUB AL,04
+;A)(+6)-(+4)
+    MOV AL,06       ;AL recebe 06 decimal
+    SUB AL,04       ;Subtrai 04 decimal de AL
 
-    MOV BL,07
-    ADD BL,02
+;B)(+7)+(+2)
+    MOV AL,07       ;AL recebe 07 decimal
+    ADD AL,02       ;Soma 02 em AL
 
-    MOV CL,06
-    NEG CL
-    MOV DS,06
-    NEG DS
-    SUB CL,DS
+;C)(-6)+(-6)
+    MOV BL,06       ;BL recebe 06 decimal
+    NEG BL          ;Negativa 06
+    MOV CL,06       ;
+    NEG CL          ;
+    ADD BL,CL       ;Faz a soma
 
-    
-    
-    
-    
+;D)(-8)-(-3)
+    MOV BH,08       ;BH recebe 08 decimal
+    NEG BH          ;Negativa 08
+    MOV BL,03       ;
+    NEG BL          ;
+    SUB BH,BL       ;Faz a subtração
+
+;Finalização do programa    
     MOV AH,4CH          ;Finaliza o programa
     INT 21H             ;Executa a finalização
 MAIN ENDP
