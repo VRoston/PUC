8b4f72470c60e56917bf0e2e52ed30a2d0912689
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-10-10 09:17:07 -0300

    Update Test1.asm

--- Assembly/Test1.asm
+++ Assembly/Test1.asm
@@ -1,25 +1,11 @@
 .MODEL SMALL
 .DATA
-DB 3,1,"AX","$"
 .CODE
 MAIN PROC
-MOV AX,@DATA
-MOV DS,AX
-    MOV BX,0FF8FH
-    MOV AX,0FFA1H
+    MOV BX,'k'
+    MOV AX,'v'
     CMP AX,BX
-    JG PULA
-    MOV CX,BX
-    LEA DX,MSG1
-    JMP IMP
-PULA:
-    MOV CX,AX
-    LEA DX,MSG2
-IMP:
-    MOV AH,,09
-    INT 21H
 
-     
 
 
 ;Finalização do programa    
