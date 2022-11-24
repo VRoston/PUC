28282229cfe5caaebc6b124cee9ac5b691c11eae
Victor de Melo Roston <111363981+VRoston@users.noreply.github.com>
2022-11-23 21:58:35 -0300

    Update Lab13.asm

--- Assembly/Lab13.asm
+++ Assembly/Lab13.asm
@@ -1,186 +1,156 @@
 TITLE NOME:VICTOR DE MELO ROSTON | RA:22006737
 
 .MODEL SMALL
-.STACK 0100h
+COR_DE_FUNDO MACRO
+ ; limpa a tela
+    MOV AH, 00       ; tipo de video
+    MOV AL, 03h      ; tipo de texto 80x25
+    INT 10H         ; executa a entrada de video
+
+ ; formata o modo de video
+    MOV AH, 09       ; escrever um caractere e atributo para a posicao do cursos
+    MOV AL, 20H      ; o caractere a mostrar
+    MOV BH, 00       ; numero da pagina
+    MOV BL, 0FH      ; atribuicao de cor
+    MOV CX, 800H     ; numero de vezes a escrever o caractere
+    INT 10H         ; executa a entrada de video
+ENDM
+
+IMPRIME_MSG MACRO
+    MOV AH, 09H      ; mostra na tela o que estivel em DL
+    INT 21H
+ENDM
+
+PULA_LINHA MACRO
+    MOV AH, 02      ; pula uma linha na impressao
+    MOV DL, 10
+    INT 21H
+ENDM
+
+EXIT_DOS MACRO
+    MOV AH, 4CH      ; encerra o programa
+    INT 21H
+ENDM
+
+.STACK 100H
 .DATA
     MSG1 DB 10, 13, '  INPUT: ' ,'$'
     MSG2 DB 10, 13, '  OUTPUT: ','$'
     
 .CODE
-
-PULA_LINHA MACRO
-    MOV AH, 02
-    MOV DL, 10
-    INT 21H
-ENDM
-
-IMPRIME_MSG MACRO
-    MOV AH,09H
-    INT 21H
-ENDM
-
-
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
-    CALL COR_DE_FUNDO
+    COR_DE_FUNDO
 
     LEA DX, MSG1
     IMPRIME_MSG
     CALL INPUT
 
-    PULA_LINHA
-
-    LEA DX, MSG2
-    IMPRIME_MSG
     CALL OUTPUT
 
-    MOV AH,4CH
-    INT 21H
+    EXIT_DOS
 MAIN ENDP
-
-; apaga o conteudo impresso na tela e muda a cor de fundo
-COR_DE_FUNDO PROC
- ; limpa a tela
-    MOV AH,00       ; tipo de video
-    MOV AL,03h      ; tipo de texto 80x25
-    INT 10H         ; executa a entrada de video
-
- ; formata o modo de video
-    MOV AH,09       ; escrever um caractere e atributo para a posicao do cursos
-    MOV AL,20H      ; o caractere a mostrar
-    MOV BH,00       ; numero da pagina
-    MOV BL,0FH      ; atribuicao de cor
-    MOV CX,800H     ; numero de vezes a escrever o caractere
-    INT 10H         ; executa a entrada de video
-
-    RET             ; retorna a funcao com a exibicao de video limpa e com a cor de fundo laranja
-COR_DE_FUNDO ENDP
-
 
 ; entrada da cadeia de caracteres
 INPUT PROC
-    PUSH BX
+ INICIO:
+    PUSH BX             ; manda para a pilha conteudo dos registradores
     PUSH CX
     PUSH DX
-@INICIO:
-; imprime prompt ?
-    ;MOV AH,2
-    ;MOV DL,'?'
-    ;INT 21H ; imprime ?
-; total = 0
-    XOR BX,BX
+    XOR BX, BX          ; inicia registradores com 0
+    XOR CX, CX           
 
-; negativo = falso
-    XOR CX,CX
-
-; le caractere
-    MOV AH,1
+    MOV AH, 01          ; caso tenha sinal (+ ou -), sera lido
     INT 21H
-; case caractere lido eh?
-    CMP AL,'-'
-    JE @NEGT
+
+    CMP AL,'-'          ; caso sinal de negativo, pula
+    JE NEGATIVO
     CMP AL,'+'
-    JE @POST
-    JMP @REP2
+    JE POSITIVO         ; caso positivo, pula
+    JMP VOLTA
 
-@NEGT:
-    MOV CX,1
-@POST:
-    INT 21H
-;end case
+ NEGATIVO:
+    MOV CX, 1           ; se for negativo, cx recebe 1
 
-@REP2:
-; if caractere esta entre 0 e 9
-    CMP AL, '0'
-    JNGE @NODIG
-    CMP AL, '9'
-    JNLE @NODIG
-; converte caractere em digito
-    AND AX,000FH
-    PUSH AX
-; total = total X 10 + digito
-    MOV AX,10
-    MUL BX          ; AX = total X 10
-    POP BX
-    ADD BX,AX       ; total - total X 10 + digito
-; le caractere
-    MOV AH,1
+ POSITIVO:
     INT 21H
-    CMP AL,13       ;CR ?
-    JNE @REP2       ; não, continua
-    ; ate CR
-    MOV AX,BX       ; guarda numero em AX
-; se negativo
-    OR CX,CX        ; negativo ?
-    JE @SAI         ; sim, sai
-; entao
+
+ VOLTA:
+    CMP AL,'0'          ; verificacao de numero real, ou caractere invelido
+    JNGE INVALIDO
+    CMP AL,'9'
+    JNLE INVALIDO
+
+    AND AX, 0FH          ; transforma em numero
+    PUSH AX             ; AX e empilhado
+
+    MOV AX, 10           ; AX recebe 10
+    MUL BX              ; multiplica BX
+    POP BX              ; Desempilha em BX
+    ADD BX, AX           ; soma BX com AX
+
+    MOV AH, 01
+    INT 21H             ; lê o numero 
+    CMP AL, 13           ; compara com o enter, caso seja igual ele para de ler 
+
+    JNE VOLTA
+
+    MOV AX, BX           ; AX<-BX
+    OR CX, CX            ; sinal
+    JE SAI              
+
     NEG AX
-; end if
-
-@SAI:
-    POP DX          ; restaura registradores
+ SAI:
+    POP DX              ; volta o valor dos registradores original antes do procedimento
     POP CX
     POP BX
-    RET             ; retorna
+    RET                 ; retorna a cadeia de caracteres lida
 
-@NODIG:
-; se caractere ilegal
-    MOV AH,2
-    MOV DL, 0DH
-    INT 21H
-    MOV DL, 0AH
-    INT 21H
-    JMP @INICIO
-
-    RET
+ INVALIDO:              ; caso caractere invalido, repete todo o processo
+    PULA_LINHA
+    JMP INICIO
 INPUT ENDP
 
 
 ; saida da cadeia de caracteres
 OUTPUT PROC
-    PUSH AX
-    PUSH BX
-    PUSH CX
-    PUSH DX
-; if AX < 0
-    OR AX,AX           ; AX < 0 ?
-    JGE @END_IF1
-;then
-    PUSH AX             ;salva o numero
-    MOV DL, '-'
-    MOV AH,2
-    INT 21H             ; imprime -
-    POP AX              ; restaura numero
-    NEG AX
-; digitos decimais
-@END_IF1:
-    XOR CX,CX           ; contador de d?gitos
-    MOV BX,10           ; divisor
-@REP1:
-    XOR DX,DX           ; prepara parte alta do dividendo
-    DIV BX              ; AX = quociente DX = resto
-    PUSH DX             ; salva resto na pilha
-    INC CX              ; contador = contador +1
-;until
-    OR AX,AX            ; quociente = 0?
-    JNE @REP1           ; nao, continua
-; converte digito em caractere
-    MOV AH,2
-; for contador vezes
-@IMP_LOOP:
-    POP DX              ; digito em DL
-    OR DL,30H
-    INT 21H
-    LOOP @IMP_LOOP
-; fim do for
-    POP DX
-    POP CX
-    POP BX
-    POP AX
+    PUSH AX             ; empilha AX para fazer um print
+    PULA_LINHA
+    LEA DX, MSG2
+    IMPRIME_MSG
+    POP AX              ; desempilha em AX
 
-    RET
+    OR AX,AX            ; sinal
+    JGE FIM
+    PUSH AX             ; se for negativo printa o sinal de menos
+    MOV AH, 02
+    MOV DL, '-'
+    INT 21H
+    POP AX
+    NEG AX
+ FIM:
+    XOR CX, CX           ; zera cx
+    MOV BX, 10
+
+ VOLTA2:            
+    XOR DX, DX           ; zera DX
+    DIV BX
+    PUSH DX             ; empilha o que esta em DX
+    INC CX              ; incrementa CX
+
+    OR AX, AX
+    JNE VOLTA2
+
+ IMPRIME:
+    POP DX              ; desempilha em DX
+    OR DL, 30H           ; transforma em caracterpara printar
+    MOV AH, 02
+    INT 21H
+    LOOP IMPRIME
+
+    RET                 ; retorna a impressao da cadeia de caracteres
 OUTPUT ENDP
 
 END MAIN
